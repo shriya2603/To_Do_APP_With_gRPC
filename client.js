6e9c38ff2dee8eab765b20a69d5076af8fff26b4
Shriya Sundriyal <starsundriyal@gmail.com>
2021-01-23 18:14:16 +0530

    added readTodosStream in client and server

--- client.js
+++ client.js
@@ -6,14 +6,42 @@ const todoPackage = grpcObject.todoPackage;
 
 const client = new todoPackage.Todo("localhost:40000",
 grpc.credentials.createInsecure());
+const text = process.argv[2];
 
 client.createTodo({
-    "id" : -1,
-    "text" : " Eating "
+    // "id" : -1,
+    "text" : text,
 },(err,response )=>{
     if(err!=null){
         console.error(err);
     }else{
-        console.log("Backend Response "+JSON.stringify(response));
+        console.log("Backend Response createTodo "+JSON.stringify(response));
     }
 });
+
+// Unary call : One request and got one Reply from backend 
+
+// In readTodos, from backend we are getting an array in a single call this would be good if size of array is smaller. But if size of array is larger then client will be crowd with huge data and it would be effect performance. Hence we will use readTodosStream which is server streaming RPC  
+// client.readTodos({},(err, response)=>{
+//     if(err!=null){
+//         console.error(err);
+//     }else{
+//         console.log("ToDos are as followed ");
+//         if(!response.items){
+//             response.items.forEach(i => {
+//                 console.log(i.text);
+//             });
+//         }
+        
+//     }
+// });
+
+const call = client.readTodosStream();
+call.on("data", item =>{
+    console.log("Recieved data from server "+JSON.stringify(item));
+});
+call.on("end", e => console.log("server done!"))
+   
+
+
+
--- server.js
+++ server.js
@@ -8,20 +8,41 @@ const todoPackage = grpcObject.todoPackage;
 const server = new grpc.Server();
 server.bind("0.0.0.0:40000", grpc.ServerCredentials.createInsecure());
 
-// mapping the methods with proto methods
+// First we get the service and then mapping the methods with proto methods
 server.addService(todoPackage.Todo.service, {
     "createTodo" : createTodo,
     "readTodos" : readTodos,
+    "readTodosStream" : readTodosStream,
 });
 
 // starts the server and begin handling requests 
 server.start();
-
-
+const todos = [];
+// Methods in grpc takes 2 parameters : call and callback 
+// call : the request we get from client 
+// callback : the repose we send back to client 
 function createTodo(call, callback){
-    console.log(call);
+    
+    console.log("Request from client: "+JSON.stringify(call));
+    if(!call || call!=null){
+        const text = call.request.text;
+        const todo ={
+            "id" : todos.length +1,
+            "text" : text,
+        }
+        todos.push(todo);
+        callback(null, todo);
+    }
+    else {
+        callback(null, "Request failed");
+    }
 }
 
 function readTodos (call, callback) {
+    callback(null,{"items": todos} )
+}
 
+function readTodosStream(call) {
+    todos.forEach(t => call.write(t));
+    call.end();
 }
